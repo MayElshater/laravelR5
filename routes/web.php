90fedfe9f0b45990aa4b0f59c2b49844aaed7274
MayMohamed <may.rashad.elshater.98@gmail.com>
2024-04-24 01:58:12 +0200

    second update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('May{id}', function ($id) {
+Route::get('May/{id}', function ($id) {
     return view('welcome the first page' . $id);
 });
